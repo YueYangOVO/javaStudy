5c9a57897c59b4fd836f94c846c64050694ae109
YueYang <loveyueyang@gmail.com>
2025-11-01 13:48:41 +0800

    rabbitMQ学习第一次提交

---
+++ rabbitMQlearn/rabbitMQ-consumer/src/main/java/com/lyy/ConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.lyy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
